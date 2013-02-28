296d66f36eb4707cbc6b5fdc23a55163ef2e4e0b
Abe Dela Cruz <aberhamDC@gmail.com>
2013-02-28 15:14:31 -0800

    interface is done, we should make it pretty

---
+++ FTAgg/src/com/ece150/ftagg/TwitterHalf.java
@@ -0,0 +1,13 @@
+package com.ece150.ftagg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TwitterHalf extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.twitside);	
+	}
+
+}
