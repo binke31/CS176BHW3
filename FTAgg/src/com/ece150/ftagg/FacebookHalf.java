33b1a7d0212b348b7bf50431494879bc06cf5243
U-Computron\Joey <Joey@Computron.(none)>
2013-02-28 17:35:23 -0800

    facebook login working, need to work on logout

--- FTAgg/src/com/ece150/ftagg/FacebookHalf.java
+++ FTAgg/src/com/ece150/ftagg/FacebookHalf.java
@@ -1,48 +1,63 @@
 package com.ece150.ftagg;
 
-import android.app.Activity;
-import android.app.DownloadManager.Request;
+import android.app.ActionBar.LayoutParams;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.facebook.Request;
 import com.facebook.Response;
 import com.facebook.Session;
 import com.facebook.SessionState;
 import com.facebook.model.GraphUser;
 
-public class FacebookHalf extends Activity {
+public class FacebookHalf extends FragmentActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fbside);	
-
+//		final TextView b = new TextView(this);
+//		b.setText("HELLO");
+//    	FacebookHalf.this.addContentView(b,new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
 		Button fbLogin = (Button) findViewById(R.id.facebookLogin);
-		//
-		//		fbLogin.setOnClickListener(new OnClickListener(){			
-		//			@Override
-		//			public void onClick(View v){ // start Facebook Login
-		//				Session.openActiveSession(this, true, new Session.StatusCallback() {					
-		//					@Override // callback when session changes state
-		//					public void call(Session session, SessionState state, Exception exception) {
-		//						if (session.isOpened()) {
-		//							Request.executeMeRequestAsync(session, new Request.GraphUserCallback() {
-		//
-		//								@Override
-		//								public void onCompleted(GraphUser user, Response response) {
-		//									if (user != null) {
-		//										//								TextView welcome = (TextView) findViewById(R.id.welcome);
-		//										//								welcome.setText("Hello " + user.getName() + "!");
-		//									}
-		//								}
-		//							});
-		//						}
-		//					}
-		//				});
-		//
-		//			}
-		//
-		//		});
+
+	
+		
+        fbLogin.setOnClickListener(new View.OnClickListener(){
+        	public void onClick(View v){
+        	    Session.openActiveSession(FacebookHalf.this, true, new Session.StatusCallback() {
+        	    	
+        	        // callback when session changes state
+        	        @Override
+        	        public void call(Session session, SessionState state, Exception exception) {
+        	          if (session.isOpened()) {
+
+        	            // make request to the /me API
+        	            Request.executeMeRequestAsync(session, new Request.GraphUserCallback() {
+
+        	              // callback after Graph API response with user object
+        	              @Override
+        	              public void onCompleted(GraphUser user, Response response) {
+        	                if (user != null) {
+        	                }
+        	              }
+        	            });
+        	          }
+        	        }
+        	      });
+        		
+        		
+        	}
+        });  
+	}
+		
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	  super.onActivityResult(requestCode, resultCode, data);
+	  Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
 	}
 }
